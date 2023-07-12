878e6a159bbaaa9ac9a35090632205aa897d3600
Sardor <henkook-baron@mail.ru>
2023-07-12 22:08:35 +0500

    edit item service

---
+++ src/modules/items/edit-item.js
@@ -0,0 +1,16 @@
+import db from '../../db/index.js';
+
+export const editItem = async ({ id, ...changes }) => {
+  const item = await db('items').where({ id }).first();
+
+  if (!item) {
+    throw new NotFoundError('Taom topilmadi');
+  }
+
+  return (
+    await db('items')
+      .where({ id })
+      .update({ ...changes })
+      .returning('*')
+  )[0];
+};
