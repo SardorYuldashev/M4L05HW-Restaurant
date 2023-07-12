507a8f59cbcac3774c7642167c059d78ae6dd357
Sardor <henkook-baron@mail.ru>
2023-07-12 22:22:44 +0500

    add itemSizesRoute

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,10 +1,11 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import usersModule from '../modules/users/_index.js';
 import clientsModule from '../modules/clients/_index.js';
-import itemsModule from '../modules/items/_index.js'
+import itemsModule from '../modules/items/_index.js';
+import itemSizesModule from '../modules/item-size/_index.js';
 
-const typdefsArr = [usersModule.typeDefs, clientsModule.typeDefs, itemsModule.typeDefs];
-const resolversArr = [usersModule.resolvers, clientsModule.resolvers, itemsModule.resolvers];
+const typdefsArr = [usersModule.typeDefs, clientsModule.typeDefs, itemsModule.typeDefs, itemSizesModule.typeDefs];
+const resolversArr = [usersModule.resolvers, clientsModule.resolvers, itemsModule.resolvers, itemSizesModule.resolvers];
 
 export const schema = makeExecutableSchema({
   typeDefs: typdefsArr,
