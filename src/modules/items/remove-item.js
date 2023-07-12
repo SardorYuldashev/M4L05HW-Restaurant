c930e450f20b7f12b32720df54cd0a291fa172ad
Sardor <henkook-baron@mail.ru>
2023-07-12 22:09:18 +0500

    remove item service

---
+++ src/modules/items/remove-item.js
@@ -0,0 +1,16 @@
+import db from '../../db/index.js';
+
+export const removeItem = async ({ id }) => {
+  const item = await db('items').where({ id }).first();
+
+  if (!item) {
+    throw new NotFoundError('Taom topilmadi');
+  };
+
+  const deleted = await db('items')
+    .where({ id })
+    .update({ is_deleted: true })
+    .returning(['id', 'type']);
+
+  return deleted[0];
+};
