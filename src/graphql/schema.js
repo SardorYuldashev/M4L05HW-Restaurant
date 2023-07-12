5b0a3b0fc987d92fd3fc7363d7832c556396b9ad
Sardor <henkook-baron@mail.ru>
2023-07-12 22:06:40 +0500

    add itemsRoute

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,9 +1,10 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import usersModule from '../modules/users/_index.js';
 import clientsModule from '../modules/clients/_index.js';
+import itemsModule from '../modules/items/_index.js'
 
-const typdefsArr = [usersModule.typeDefs, clientsModule.typeDefs];
-const resolversArr = [usersModule.resolvers, clientsModule.resolvers];
+const typdefsArr = [usersModule.typeDefs, clientsModule.typeDefs, itemsModule.typeDefs];
+const resolversArr = [usersModule.resolvers, clientsModule.resolvers, itemsModule.resolvers];
 
 export const schema = makeExecutableSchema({
   typeDefs: typdefsArr,
